6df50387fb5565d4dd09349ba52a0ae04859cd72
arno <a.dreschnig@simstation.com>
2019-04-10 14:47:07 +0200

    simstation changes

---
+++ NAudio/Wave/WaveStreams/WaveStreamDescriptor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NAudio.Wave
+{
+   /// <summary>
+   /// Additional meta data for wave stream (arno 2018)
+   /// </summary>
+   public class WaveStreamDescriptor
+   {
+      /// <summary>
+      /// Name
+      /// </summary>
+      public string Name { get; set; }
+
+      /// <summary>
+      /// ChannelOrigin
+      /// </summary>
+      public int[] Channels { get; set; }
+
+      /// <summary>
+      /// TrainingRoomId
+      /// </summary>
+      public string TrainingRoomId { get; set; }
+   }
+}
